5c4f72299f52462da8aebf831f3c057b709780e1
shubhamr10 <shubhamrauniyar10@gmail.com>
2024-05-15 16:25:16 +0545

    [React] Assignment to develop a profile card

--- React/jonas_schedtmann/ultimate-react-course/03-pizza-menu/starter/src/index.js
+++ React/jonas_schedtmann/ultimate-react-course/03-pizza-menu/starter/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -49,23 +50,66 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <br />
+      <br />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizaa check " />
-      <h2>Pizza Prosciutto</h2>
-      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    <header className="header">
+      <h1
+      // style={{ color: "red", fontSize: "50px", textTransform: "uppercase" }}
+      >
+        Pizza Palace Co.
+      </h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Menu</h2>
+      <Pizza
+        name="Pizza asasas"
+        ingredients="Tomato, "
+        photoName="pizzas/spinaci.jpg"
+        price={12}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt="Pizaa check " />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
   );
 }
+const Footer = () => {
+  const hours = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hours >= openHour && hours < closeHour;
+  const message = isOpen ? "We are open" : "We are closed";
+  console.log(message);
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}@Pizza Palace Co. 2021
+    </footer>
+  );
+  //   return React.createElement("footer", null, "@Pizza Palace Co. 2021");
+};
 
 // React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
