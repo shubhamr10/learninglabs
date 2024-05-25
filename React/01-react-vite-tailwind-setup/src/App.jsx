1de2a4f5db6d581dbf1e95b32451461c3b506a2e
shubhamr10 <shubhamrauniyar10@gmail.com>
2024-05-25 17:32:28 +0545

    chore: Add Tailwind CSS setup and configuration files

---
+++ React/01-react-vite-tailwind-setup/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+function App() {
+  return (
+    <div className="container">
+      <Login />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/01-react-vite-tailwind-setup/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <header className="font-sans">Your Github Repositories</header>;
+}
+
+export default Header;
