dc79125b61d00a5a5ba5ffcadb2a4d30d7281218
shubhamr10 <shubhamrauniyar10@gmail.com>
2024-08-19 18:32:48 +0530

    added redux dev tools

--- React/ZTM-Complete_react/crwn-clothing/src/store/store.js
+++ React/ZTM-Complete_react/crwn-clothing/src/store/store.js
@@ -1,32 +1,61 @@
 import { compose, applyMiddleware, legacy_createStore } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-import { rootReducer } from "./rootReducer";
+import { rootReducer} from "./rootReducer";
 
-const loggerMiddleware  = store => next => action => {
-    if(!action.type){
-        return next(action);
-    }
-
-    console.log("type: ", action.type);
-    console.log("payload: ", action.payload);
-    next(action);
-    console.log("next state: ", store.getState());
-}
-
-const persistConfigure = {
+const persistConfig = {
     key:"root",
     storage,
-    blacklist:["user"],
+    blacklist:["user"]
 }
 
-const persistedReducer = persistReducer(persistConfigure, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
 
-const middlewares = [loggerMiddleware];
-const composedEnchaner = compose(applyMiddleware(...middlewares));
+// setup for redux devtools extensions
+const composedEnchancer = (process.env.NODE_ENV === "prod" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-export const store = legacy_createStore(persistedReducer, undefined, composedEnchaner);
+const composedEnchancers = composedEnchancer(applyMiddleware(...middleWares));
+
+export const store = legacy_createStore(
+    persistedReducer,
+    undefined,
+    composedEnchancers
+)
 
 export const persistor = persistStore(store);
+
+// import { compose, applyMiddleware, legacy_createStore } from "redux";
+// // import logger from "redux-logger";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+
+// import { rootReducer } from "./rootReducer";
+
+// const loggerMiddleware  = store => next => action => {
+//     if(!action.type){
+//         return next(action);
+//     }
+
+//     console.log("type: ", action.type);
+//     console.log("payload: ", action.payload);
+//     next(action);
+//     console.log("next state: ", store.getState());
+// }
+
+// const persistConfigure = {
+//     key:"root",
+//     storage,
+//     blacklist:["user"],
+// }
+
+// const persistedReducer = persistReducer(persistConfigure, rootReducer);
+
+// const middlewares = [loggerMiddleware];
+// const composedEnchaner = compose(applyMiddleware(...middlewares));
+
+// export const store = legacy_createStore(persistedReducer, undefined, composedEnchaner);
+
+// export const persistor = persistStore(store);
