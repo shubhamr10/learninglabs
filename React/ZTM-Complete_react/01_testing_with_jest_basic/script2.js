2cefa648d67007f456f83a3978e9dd9c50217640
shubhamr10 <shubhamrauniyar10@gmail.com>
2024-09-11 12:43:07 +0530

    Added tests with jest

---
+++ React/ZTM-Complete_react/01_testing_with_jest_basic/script2.js
@@ -0,0 +1,29 @@
+// writing asynchronous code
+// http://swapi.py4e.com/api/people
+const fetch = require("node-fetch");
+
+
+// using dependencies injection here.
+const getPeoplePromise = fetch => {
+    return fetch("http://swapi.py4e.com/api/people")
+        .then(response => response.json())
+        .then(data => {
+            return {
+                count: data.count,
+                results: data.results
+            }
+        });
+};
+
+const getPeople = async fetch =>{
+    const getRequest = await fetch("http://swapi.py4e.com/api/people");
+    const data = await getRequest.json();
+    return {
+        count: data.count,
+        results: data.results
+    }
+}
+module.exports = {
+    getPeoplePromise,
+    getPeople
+}
